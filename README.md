a53b26dc53fd4d2cb681700122b1e99def342d2c
Dat Le <dat.le@sap.com>
2017-11-19 17:08:12 +0700

    Updated layout of tag pages. Added more gulp commands. Formatted code files.
    Change-Id: I81599b91c375b52de769b2b580acb3a9b2b3fd6b

---
+++ README.md
@@ -0,0 +1,15 @@
+Blog - Dat Le
+===
+
+1. Prerequisites
+  * Ruby: https://rubyinstaller.org/downloads/
+
+2. How to run.
+  $ gulp install
+  $ gulp serve
+
+3. Rebuild tags and categories:
+  $ gulp build
+
+4. Deploy to GitHub Pages:
+  * commit all files in /tag folder.
--- gulpfile.js
+++ gulpfile.js
@@ -6,78 +6,94 @@ var fs = require('fs');
 var path = require('path');
 
 var run = function (command, cb) {
-    var child = spawn('cmd', ['/c', command]);
-    child.stdout.setEncoding('utf8');
-    child.stdout.on('data', function (data) {
-        gutil.log(data.trim());
-    });
+  var child = spawn('cmd', ['/c', command]);
+  child.stdout.setEncoding('utf8');
+  child.stdout.on('data', function (data) {
+    gutil.log(data.trim());
+  });
 
-    child.stderr.setEncoding('utf8');
-    child.stderr.on('data', function (data) {
-        gutil.log(gutil.colors.red(data));
-    });
+  child.stderr.setEncoding('utf8');
+  child.stderr.on('data', function (data) {
+    gutil.log(gutil.colors.red(data));
+  });
 
-    child.on('close', function(code) {
-        cb && cb(code);
-    });
+  child.on('close', function (code) {
+    cb && cb(code);
+  });
 };
 
 var rebuildTags = function () {
-    var tags = {};
-    
-    fs.readdirSync('./_posts').forEach(file => {
-        var rgxMatchFilename = /\d{4}-\d{2}-\d{2}-.+\..+/ig;
-        var rgxMatchTags = /---[^]*tags:\s*\[(.+?)\][^]*---/igm;
-        
-        if (!file.match(rgxMatchFilename)) {
-            return;
-        }
+  var tags = {};
 
-        var match =  rgxMatchTags.exec(fs.readFileSync(path.join('./_posts', file), 'utf8'));
-        if (match && match[1]) {
-            match[1].split(',').forEach(tag => tags[friendlyUrl(tag.trim())] = tag.trim());
-        }
-    });
+  fs.readdirSync('./_posts').forEach(file => {
+    var rgxMatchFilename = /\d{4}-\d{2}-\d{2}-.+\..+/ig;
+    var rgxMatchTags = /---[^]*tags:\s*\[(.+?)\][^]*---/igm;
 
-    return tags;
+    if (!file.match(rgxMatchFilename)) {
+      return;
+    }
+
+    var match = rgxMatchTags.exec(fs.readFileSync(path.join('./_posts', file), 'utf8'));
+    if (match && match[1]) {
+      match[1].split(',').forEach(tag => tags[friendlyUrl(tag.trim())] = tag.trim());
+    }
+  });
+
+  return tags;
 };
 
 var friendlyUrl = function (url) {
-    return url.replace(/#/ig, 'sharp')
-        .replace(/\./ig, 'dot');
+  return url.replace(/#/ig, 'sharp')
+    .replace(/\./ig, 'dot');
 };
 
 
 gulp.task('server', ['serve']);
-gulp.task('serve', function(cb) {
-    run('bundle exec jekyll serve --watch');
+gulp.task('serve', function (cb) {
+  run('bundle exec jekyll serve --watch');
 });
 
 gulp.task('rebuild-tags', function (cb) {
-    var content = '', tags = rebuildTags();
-    for (var tag in tags) {
-        content += tag + ': ' + tags[tag] + '\n';
-        fs.writeFileSync('./tag/' + tag + '.md',
-`---
+  var content = '',
+    tags = rebuildTags();
+  for (var tag in tags) {
+    content += tag + ': ' + tags[tag] + '\n';
+    fs.writeFileSync('./tag/' + tag + '.md',
+      `---
 layout: tag
 title: Post with tag "{title}"
 tag: {tag}
 permalink: /tag/{tag-url}/
 ---`.replace('{title}', tags[tag]).replace('{tag}', tags[tag]).replace('{tag-url}', tag)
-        );
-    }
-    fs.writeFileSync('./_data/tags.yml', content);
+    );
+  }
+  fs.writeFileSync('./_data/tags.yml', content);
 });
 
 gulp.task('clean-tags', function (cb) {
-    gulp.src('./tag/**/*.md', {read: false})
-        .pipe(clean());
+  gulp.src('./tag/**/*.md', {
+      read: false
+    })
+    .pipe(clean());
 });
+
+gulp.task('clean-sites', function (cb) {
+  gulp.src('./_site/**/*.*', {
+      read: false
+    })
+    .pipe(clean());
+});
+
+gulp.task('clean', ['clean-sites', 'clean-tags']);
 
 gulp.task('build', ['rebuild-tags'], function (cb) {
-    run('bundle exec jekyll build');
+  run('bundle exec jekyll build', cb);
 });
 
-gulp.task('install', function(cb) {
-    run('bundle install');
+gulp.task('install-bundler', function (cb) {
+  run('gem install bundler', cb)
+});
+
+gulp.task('install', ['install-bundler'], function (cb) {
+  run('bundle install', cb);
 });
