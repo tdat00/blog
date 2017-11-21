ad30d5e0eb76faae286271d1bd42c31043985107
Dat Le <dat.le@sap.com>
2017-11-21 11:55:49 +0700

    Added category pages. Fixed no comments count in post detail page.
    Change-Id: I8e8c57e3f9bb9d8fc7c892e19291215a7583dd0c

--- gulpfile.js
+++ gulpfile.js
@@ -26,8 +26,8 @@ var rebuildTags = function () {
   var tags = {};
 
   fs.readdirSync('./_posts').forEach(file => {
-    var rgxMatchFilename = /\d{4}-\d{2}-\d{2}-.+\..+/ig;
-    var rgxMatchTags = /---[^]*tags:(.*?)\r?\n[^]*---/igm;
+    var rgxMatchFilename = /\d{4}-\d{2}-\d{2}-.+\.md$/ig;
+    var rgxMatchTags = /---[^]*tags:\s*\[?(.*?)\]?\s*\r?\n[^]*---/igm;
 
     if (!file.match(rgxMatchFilename)) {
       return;
@@ -35,7 +35,7 @@ var rebuildTags = function () {
 
     var match = rgxMatchTags.exec(fs.readFileSync(path.join('./_posts', file), 'utf8'));
     if (match && match[1]) {
-      match[1].split(/[,\s]/ig).forEach(tag => {
+      match[1].split(/[,\s]+/ig).forEach(tag => {
         if (tag.trim()) {
           tags[friendlyUrl(tag.trim())] = tag.trim();
         }
@@ -45,37 +45,89 @@ var rebuildTags = function () {
   return tags;
 };
 
+var rebuildCategories = function () {
+  var categories = {};
+
+  fs.readdirSync('./_posts').forEach(file => {
+    var rgxMatchFilename = /\d{4}-\d{2}-\d{2}-.+\.md$/ig;
+    var rgxMatchCategories = /---[^]*categories:\s*\[?(.*?)\]?\s*\r?\n[^]*---/igm;
+
+    if (!file.match(rgxMatchFilename)) {
+      return;
+    }
+
+    var match = rgxMatchCategories.exec(fs.readFileSync(path.join('./_posts', file), 'utf8'));
+    if (match && match[1]) {
+      var cats = match[1].split(/[,\s]+/ig);
+      for (var i = 1; i <= cats.length; i++) {
+        categories[cats.slice(0, i).join('/')] = cats.slice(0, i).join(' ');
+      }
+    }
+  });
+  return categories;
+};
+
 var friendlyUrl = function (url) {
   return url.replace(/#/ig, 'sharp')
     .replace(/\./ig, 'dot');
 };
 
+var writeFile = function (filename, content) {
+  var folder = path.dirname(filename);
+  if (!fs.existsSync(folder)) {
+    fs.mkdirSync(folder);
+  }
+  fs.writeFileSync(filename, content);
+};
 
 gulp.task('server', ['serve']);
 gulp.task('serve', function (cb) {
   run('bundle exec jekyll serve --watch');
 });
 
-gulp.task('rebuild-tags', function (cb) {
+gulp.task('build-tags', function (cb) {
   var content = '',
     tags = rebuildTags();
-  for (var tag in tags) {
-    content += tag + ': ' + tags[tag] + '\n';
-    fs.writeFileSync('./tag/' + tag + '.md',
+  for (var tagUrl in tags) {
+    content += tagUrl + ': ' + tags[tagUrl] + '\n';
+    writeFile('./tag/' + tagUrl + '.md',
       `---
 layout: tag
 title: Posts with tag '{tag}'
 tag: {tag}
 permalink: /tag/{tag-url}/
----`.replace(/\{tag\}/ig, tags[tag]).replace('{tag-url}', tag)
+---`.replace(/\{tag\}/ig, tags[tagUrl]).replace(/\{tag-url\}/ig, tagUrl)
     );
   }
-  fs.writeFileSync('./_data/tags.yml', content);
+  writeFile('./_data/tags.yml', content);
+  cb && cb();
+});
+
+gulp.task('build-categories', function (cb) {
+  var categories = rebuildCategories();
+  for (var categoryUrl in categories) {
+    writeFile('./category/' + categoryUrl + '.md',
+      `---
+layout: category
+title: Posts in category '{category-url}'
+category: {category}
+permalink: /{category-url}/
+---`.replace(/\{category\}/ig, categories[categoryUrl]).replace(/\{category-url\}/ig, categoryUrl)
+    );
+  }
   cb && cb();
 });
 
 gulp.task('clean-tags', function (cb) {
   gulp.src('./tag/**/*.md', {
+      read: false
+    })
+    .pipe(clean());
+    cb && cb()
+});
+
+gulp.task('clean-categories', function (cb) {
+  gulp.src('./category', {
       read: false
     })
     .pipe(clean());
@@ -90,11 +142,11 @@ gulp.task('clean-sites', function (cb) {
   cb && cb()
 });
 
-gulp.task('clean', ['clean-sites', 'clean-tags'], function (cb) {
+gulp.task('clean', ['clean-sites', 'clean-tags', 'clean-categories'], function (cb) {
   cb && cb();
 });
 
-gulp.task('build', ['rebuild-tags'], function (cb) {
+gulp.task('build', ['build-tags', 'build-categories'], function (cb) {
   run('bundle exec jekyll build', cb);
 });
 
